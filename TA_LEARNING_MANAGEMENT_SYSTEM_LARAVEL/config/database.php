a5200c95aabcc8bafd62ebe951a52a063923d562
JG1van <gian123ivan@gmail.com>
2026-05-21 11:24:58 +0700

    Update database.php

--- TA_LEARNING_MANAGEMENT_SYSTEM_LARAVEL/config/database.php
+++ TA_LEARNING_MANAGEMENT_SYSTEM_LARAVEL/config/database.php
@@ -61,6 +61,7 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+            'timezone' => '+07:00',
         ],
         // Database terpisah untuk logging (quiz activity, dll)
         'mysql_log' => [
@@ -81,6 +82,7 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+            'timezone' => '+07:00',
         ],
 
         'pgsql' => [
