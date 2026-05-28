5767b7e3e674d917d6b4741e915cec02df07c1fe
JG1van <gian123ivan@gmail.com>
2026-05-28 19:17:20 +0700

    Update Kernel.php

--- TA_LEARNING_MANAGEMENT_SYSTEM_LARAVEL/app/Console/Kernel.php
+++ TA_LEARNING_MANAGEMENT_SYSTEM_LARAVEL/app/Console/Kernel.php
@@ -20,7 +20,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('serial:send-expiry-reminder')
-            ->dailyAt('8:00');
+            ->dailyAt('19:20');
         // * * * * * php /path-ke-project/artisan schedule:run >> /dev/null 2>&1
 
         // Reset cache lock setiap tengah malam
