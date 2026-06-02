072e3b1055ab26996fad5b8a51332d6b35446e05
JG1van <gian123ivan@gmail.com>
2026-06-02 22:00:42 +0700

    Update edit.blade.php

--- TA_LEARNING_MANAGEMENT_SYSTEM_LARAVEL/resources/views/admin/produk/edit.blade.php
+++ TA_LEARNING_MANAGEMENT_SYSTEM_LARAVEL/resources/views/admin/produk/edit.blade.php
@@ -97,7 +97,8 @@
     </form>
 
     {{-- Modal Tambah Materi --}}
-    <div class="modal fade p-5" id="modalMateri" tabindex="-1" aria-labelledby="modalMateriLabel" aria-hidden="true">
+    <div class="modal fade p-5" id="modalMateri" tabindex="-1" aria-labelledby="modalMateriLabel" aria-hidden="true"
+        style="overflow-x: scroll;">
         <div class="modal-dialog modal-md mt-5 modal-dialog-scrollable">
             <div class="modal-content">
                 <div class="modal-header">
