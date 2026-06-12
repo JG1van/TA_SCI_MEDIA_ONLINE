3c1724e13b855696008c400a942da28725a75332
JG1van <gian123ivan@gmail.com>
2026-06-12 20:26:44 +0700

    Update index.blade.php

--- TA_LEARNING_MANAGEMENT_SYSTEM_LARAVEL/resources/views/admin/admin/index.blade.php
+++ TA_LEARNING_MANAGEMENT_SYSTEM_LARAVEL/resources/views/admin/admin/index.blade.php
@@ -199,7 +199,7 @@
             <tbody id="adminBody">
                 @forelse ($admins as $admin)
                     <tr id="row{{ $admin->id }}">
-                        <td>{{ $admin + 1 }}</td>
+                        <td>{{ $loop->iteration }}</td>
                         <td class="admin-name">{{ $admin->name }}</td>
                         <td>{{ $admin->username }}</td>
                         <td class="text-center align-middle">
